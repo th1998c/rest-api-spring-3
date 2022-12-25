1aefe2668380a3aa0637a03f75c951b704940927
Thiago Carneiro da Silva <thiagocarneiro1998@gmail.com>
2022-12-25 20:22:38 -0300

    return id of doctor in doctorRecord

--- src/main/java/com/th/apispring3/model/record/DoctorRecord.java
+++ src/main/java/com/th/apispring3/model/record/DoctorRecord.java
@@ -3,9 +3,9 @@ package com.th.apispring3.model.record;
 import com.th.apispring3.model.Doctor;
 import com.th.apispring3.model.Enum.Specialty;
 
-public record DoctorRecord(String name, String email, String crm, Specialty specialty){
+public record DoctorRecord(Long id, String name, String email, String crm, Specialty specialty){
 	
 	public DoctorRecord(Doctor doctor) {
-		this(doctor.getName(), doctor.getEmail(), doctor.getCrm(), doctor.getSpecialty());
+		this(doctor.getId(), doctor.getName(), doctor.getEmail(), doctor.getCrm(), doctor.getSpecialty());
 	}
 }
